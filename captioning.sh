bdc2afa2e1dd1c9345bc4129454f438d0c276523
Luca Paladino <luca.paladino26@gmail.com>
2026-07-01 17:13:03 +0200

    Reduced time for captioning

--- captioning.sh
+++ captioning.sh
@@ -7,7 +7,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=40G
-#SBATCH --time=24:00:00
+#SBATCH --time=4:00:00
 #SBATCH --constraint=gpu_A40_45G|gpu_L40S_45G|gpu_RTX_A5000_24G|gpu_RTX6000_24G
 
 # Caricamento moduli
